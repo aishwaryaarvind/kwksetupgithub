bf7342614c1660c2e3303b367ddca75ee316e965
aishwaryaarvind <35902121+aishwaryaarvind@users.noreply.github.com>
2020-07-28 10:47:52 -0400

    yay first one

--- kwksetupgithub/ViewController.swift
+++ kwksetupgithub/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        func viewDidLoad() {         super.viewDidLoad();         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background")!) }
+
     }
 
 
